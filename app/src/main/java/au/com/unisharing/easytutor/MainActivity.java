602d8c6cb4609ff4fd699a916d255ff978ca13ee
ppbao <bruce.liu.02@gmail.com>
2016-05-10 12:09:53 +1000

    Initial commit

---
+++ app/src/main/java/au/com/unisharing/easytutor/MainActivity.java
@@ -0,0 +1,18 @@
+package au.com.unisharing.easytutor;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+/**
+ * Copyright @Unisharing
+ *
+ * created by bruce
+ */
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
